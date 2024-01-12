a88f76b149d93350740de00d908d950284826a9f
siyanaparveen <ziyanaparveen6@gmail.com>
2024-01-12 22:27:06 +0300

    initial commit

---
+++ src/constants/constants.js
@@ -0,0 +1,5 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+
+export const API_KEY = "c772a1c372bcd8a780e8f51d2e8d1756";
+
+export const imageUrl = 'https://image.tmdb.org/t/p/original';
